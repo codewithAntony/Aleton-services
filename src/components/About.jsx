3a193380288b7d35d9a1fa5088c802b911781ec4
Antony Murithi <antonymurithi51@gmail.com>
2024-07-31 23:25:59 +0300

    about components updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 function About() {
     return (
-        <div className="bg-[#FFD43A] py-[55px]">
+        <div className="bg-[#FFD43A] py-[80px]">
             <div className="w-full mx-auto p-3 max-w-6xl">
                 <div className="flex flex-col sm:flex-row sm:gap-[190px]">
                     <div className="w-full md:w-1/2 lg:w-2/3">
@@ -12,8 +12,8 @@ function About() {
                         </h1>
                     </div>
                 </div>
-                <div className="flex flex-col sm:flex sm:flex-row sm:gap-[30px]">
-                    <div className="border border-gray-200 w-[320px] h-[180px] p-5 sm:w-[450px] sm:h-[220px] sm:p-10">
+                <div className="flex flex-col py-8  sm:flex sm:flex-row sm:gap-[40px]">
+                    <div className="border border-black w-[320px] h-[200px] mb-[30px] p-5 hover:bg-white sm:w-[500px] sm:h-[230px] sm:p-10">
                         <svg
                             className="w-6 h-6 text-gray-800 dark:text-white"
                             aria-hidden="true"
@@ -29,13 +29,13 @@ function About() {
                                 clip-rule="evenodd"
                             />
                         </svg>
-                        <p>
+                        <p className="py-3">
                             Lorem ipsum dolor sit amet consectetur, adipisicing
                             elit. Quam beatae ratione voluptatibus consectetur
                             numquam voluptatum! Ut atque quisquam sint illum?
                         </p>
                     </div>
-                    <div className="border  border-gray-200 w-[320px] h-[180px] p-5 sm:w-[450px] sm:h-[220px] sm:p-10">
+                    <div className="border border-black w-[320px] h-[200px] p-5 sm:w-[500px] sm:h-[230px] sm:p-10 hover:bg-white">
                         <svg
                             className="w-6 h-6 text-gray-800 dark:text-white"
                             aria-hidden="true"
@@ -51,11 +51,108 @@ function About() {
                                 clip-rule="evenodd"
                             />
                         </svg>
-                        <p>
+                        <p className="py-3">
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. Eos doloremque amet vel officiis libero autem
                             repudiandae sit totam aperiam error.
                         </p>
+                    </div>
+                </div>
+                <div className="bg-white py-[60px] my-[30px]">
+                    <div className="py-[10px] flex flex-col p-5">
+                        <h5 className="text-[#5359B9] font-semibold text-lg pb-[15px]">
+                            Like what you see?
+                        </h5>
+                        <h1 className="pb-[15px] text-black font-bold text-3xl">
+                            Lets Work Together
+                        </h1>
+                    </div>
+                    <div className="flex flex-col gap-7 sm:gap-[210px] p-3 sm:flex sm:flex-row sm:p-7">
+                        <p className="pl-2 text-gray-500 font-medium">
+                            We are currently available for work, <br /> so if
+                            you'd like to discuss any projects <br /> you can
+                            reach us through any of the <br /> available contact
+                            information.
+                        </p>
+                        <div className="flex flex-col gap-7">
+                            <div className="flex gap-5">
+                                <svg
+                                    className="w-[27px] h-[27px] text-[#5359B9] dark:text-white"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M21 8v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8m18 0-8.029-4.46a2 2 0 0 0-1.942 0L3 8m18 0-9 6.5L3 8"
+                                    />
+                                </svg>
+                                <p>antonymurithi51@gmail.com</p>
+                            </div>
+                            <div className="flex gap-5">
+                                <svg
+                                    className="w-[27px] h-[27px] text-[#5359B9]  dark:text-white"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M9 17h6l3 3v-3h2V9h-2M4 4h11v8H9l-3 3v-3H4V4Z"
+                                    />
+                                </svg>
+                                <p>+254 725 010 898</p>
+                            </div>
+                            <div className="flex gap-5">
+                                <svg
+                                    className="w-[27px] h-[27px] text-[#5359B9] dark:text-white"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    fill="currentColor"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z"
+                                        clip-rule="evenodd"
+                                    />
+                                    <path d="M7.2 8.809H4V19.5h3.2V8.809Z" />
+                                </svg>
+                                <p>Linked In</p>
+                            </div>
+                            <div className="flex gap-5">
+                                <svg
+                                    className="w-[27px] h-[27px] text-[#5359B9] dark:text-white"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    fill="currentColor"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z"
+                                        clip-rule="evenodd"
+                                    />
+                                </svg>
+                                <p>Github</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
