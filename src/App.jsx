61aa4232ad394ae924a1b09c269a5332576d2ece
Antony Murithi <antonymurithi51@gmail.com>
2024-09-04 15:27:03 +0300

    detailed skills component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Header from './components/Header';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Reviews from './components/Reviews';
+import DetailedSkills from './components/DetailedSkills';
 
 function App() {
     return (
         <BrowserRouter>
             <Header />
             <Skills />
+            <DetailedSkills />
             <Projects />
             <Reviews />
             <About />
