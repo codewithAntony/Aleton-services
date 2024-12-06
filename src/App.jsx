078b001e62005d29e171c343fded670e9e21d4f9
Antony Murithi <antonymurithi51@gmail.com>
2024-12-06 21:50:11 +0300

    update my skills component

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ import Header from './components/Header';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Reviews from './components/Reviews';
-import DetailedSkills from './components/DetailedSkills';
 
 function App() {
     return (
         <BrowserRouter>
             <Header />
             <Skills />
-            <DetailedSkills />
             <Projects />
             <Reviews />
             <About />
